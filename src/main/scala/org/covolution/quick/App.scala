19e3635dc2be5c0773b4cc608b1bbdeb6812f89a
Gethin James <gethin.james@alfresco.com>
2015-12-21 11:55:53 +0100

    Added cassandra with connector 1.5.0-M3

--- src/main/scala/org/covolution/quick/App.scala
+++ src/main/scala/org/covolution/quick/App.scala
@@ -3,6 +3,9 @@ package org.covolution.quick
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+
 /**
  * @author Gethin James
  * Try this:
@@ -15,14 +18,24 @@ object App {
     val conf = new SparkConf()
       .setAppName("The Squick Spark app listening 1")
       .setMaster("local[*]")
+      .set("spark.cassandra.connection.host", "cassa")
     val sc = new SparkContext(conf)
 
-    val data = Seq(
-      """{"user":"helena","commits":98, "month":3, "year":2015}""",
-      """{"user":"jacek-lewandowski", "commits":72, "month":3, "year":2015}""",
-      """{"user":"pkolaczk", "commits":42, "month":3, "year":2015}""")
+    CassandraConnector(conf).withSessionDo { session =>
+      session.execute("DROP KEYSPACE IF EXISTS ctest")
+      session.execute("CREATE KEYSPACE ctest WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
+      session.execute("CREATE TABLE ctest.kv(key text PRIMARY KEY, value int);")
+      session.execute("INSERT INTO ctest.kv(key, value) VALUES ('key1', 1);")
+      session.execute("INSERT INTO ctest.kv(key, value) VALUES ('key2', 2);")
+    }
 
-    data.foreach (println)
+    val rdd = sc.cassandraTable("ctest", "kv")
+    println(rdd.count)
+    println(rdd.first)
+    println(rdd.map(_.getInt("value")).sum)
+
+    val collection = sc.parallelize(Seq(("key3", 3), ("key4", 4)))
+    collection.saveToCassandra("ctest", "kv", SomeColumns("key", "value"))
   }
 
 }
